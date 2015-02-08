182db790807bc0711c460acbad2829a4184cb665
manoelbo <manoelbo@hotmail.com>
2015-02-08 14:24:15 +0100

    Init Rep

---
+++ run.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, jsonify, url_for, json, Response, make_response, request
+import os
+ 
+app = Flask(__name__)    
+
+
+
+######## INDEX ########
+ 
+@app.route('/')
+def home():
+  return render_template('index.html')
+
+
+####### GET ########
+
+@app.route('/todos/api', methods=['GET'])
+def get_tasks():
+	tasks = [
+  		{"task": u"Do the Laundry"},
+  		{"task": u"Learn Python"}
+	]
+	return Response(json.dumps(tasks),  mimetype='application/json')
+    
+
+
+ ####### POST ########
+
+@app.route('/todo/api/v1.0/tasks', methods=['POST'])
+def create_task():
+    tasks = {
+        'task': request.json.get('task', ""),
+    }
+    tasks.append(task)
+    return jsonify({'task': task}), 201
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+if __name__ == '__main__':
+  app.run(debug=True)
