0e335f21aba9af8503b514ebdda2a9d3f7780c0b
manoelbo <manoelbo@hotmail.com>
2015-02-10 02:27:54 +0100

    adding task counter/mark all/css

--- run.py
+++ run.py
@@ -4,9 +4,9 @@ import os
 app = Flask(__name__)    
 
 tasks = [
-  	{"task": u"Do the Laundry"},
-  	{"task": u"Learn Python"},
-    {"task": u"JorgeBen"}
+  	{"task": u"Discuss report with John", 'status': 0, "id": 1},
+  	{"task": u"Learn Python", 'status': 0, "id": 2},
+    {"task": u"JorgeBen", 'status': 0, "id": 3}
 		]
 
 ######## INDEX ########
@@ -24,22 +24,50 @@ def home():
 def get_tasks():
     return jsonify({'tasks': tasks})
 
-@app.route('/todos/api<int:task_id>', methods=['GET'])
+
+############# POST ########
+
+
+@app.route('/todos/api', methods=['POST'])
+def create_task():
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'task': request.json['task'],
+        'status': 0, 
+    }
+    tasks.append(task)
+    return jsonify({'task': task}), 201
+
+
+############# POST ########
+
+@app.route('/todos/api/<int:task_id>', methods=['GET'])
 def get_task(task_id):
     task = [task for task in tasks if task['id'] == task_id]
     if len(task) == 0:
         abort(404)
     return jsonify({'task': task[0]})
 
+############## PUT ############
 
-@app.route('/todos/api', methods=['POST'])
-def create_task():
-    task = {
-        'task': request.json['task'],
-    }
+@app.route('/todos/api<int:task_id>', methods=['PUT'])
+def update_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'title' in request.json and type(request.json['title']) != unicode:
+        abort(400)
+    if 'description' in request.json and type(request.json['description']) is not unicode:
+        abort(400)
+    if 'done' in request.json and type(request.json['done']) is not bool:
+        abort(400)
+    task[0]['title'] = request.json.get('title', task[0]['title'])
+    task[0]['description'] = request.json.get('description', task[0]['description'])
+    task[0]['done'] = request.json.get('done', task[0]['done'])
+    return jsonify({'task': task[0]})
 
-    tasks.append(task)
-    return jsonify({'task': task}), 201
 
 
 
