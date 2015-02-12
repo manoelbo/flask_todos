559180b81e3a796bea384c8e366ff9cb7940e4cf
Manoel Brasil Orlandi <manoelbo@hotmail.com>
2015-02-12 01:09:32 +0100

    update status/mark all working

--- run.py
+++ run.py
@@ -4,9 +4,9 @@ import os
 app = Flask(__name__)    
 
 tasks = [
-  	{"task": u"Discuss report with John", 'status': 0, "id": 1},
-  	{"task": u"Learn Python", 'status': 0, "id": 2},
-    {"task": u"JorgeBen", 'status': 0, "id": 3}
+  	{"task": u"Discuss report with John", 'status': False, "id": 1},
+  	{"task": u"Learn Python", 'status': False, "id": 2},
+    {"task": u"Buy Milk", 'status': False, "id": 3}
 		]
 
 ######## INDEX ########
@@ -24,6 +24,13 @@ def home():
 def get_tasks():
     return jsonify({'tasks': tasks})
 
+@app.route('/todos/api/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
+
 
 ############# POST ########
 
@@ -33,39 +40,21 @@ def create_task():
     task = {
         'id': tasks[-1]['id'] + 1,
         'task': request.json['task'],
-        'status': 0, 
+        'status': False, 
     }
     tasks.append(task)
     return jsonify({'task': task}), 201
 
 
-############# POST ########
 
-@app.route('/todos/api/<int:task_id>', methods=['GET'])
-def get_task(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
-    if len(task) == 0:
-        abort(404)
-    return jsonify({'task': task[0]})
 
 ############## PUT ############
 
-@app.route('/todos/api<int:task_id>', methods=['PUT'])
+@app.route('/todos/api/<int:task_id>', methods=['PUT'])
 def update_task(task_id):
     task = [task for task in tasks if task['id'] == task_id]
-    if len(task) == 0:
-        abort(404)
-    if not request.json:
-        abort(400)
-    if 'title' in request.json and type(request.json['title']) != unicode:
-        abort(400)
-    if 'description' in request.json and type(request.json['description']) is not unicode:
-        abort(400)
-    if 'done' in request.json and type(request.json['done']) is not bool:
-        abort(400)
-    task[0]['title'] = request.json.get('title', task[0]['title'])
-    task[0]['description'] = request.json.get('description', task[0]['description'])
-    task[0]['done'] = request.json.get('done', task[0]['done'])
+    task[0]['task'] = request.json.get('task', task[0]['task'])
+    task[0]['status'] = request.json.get('status', task[0]['status'])
     return jsonify({'task': task[0]})
 
 
